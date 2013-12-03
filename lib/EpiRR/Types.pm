baf014dcd8175f41dfec336393ca7bedf3b111e2
davidrichardson <david.m.richardson@gmail.com>
2013-12-03 14:19:09 +0000

    Use role_types to define interface

--- lib/EpiRR/Types.pm
+++ lib/EpiRR/Types.pm
@@ -6,3 +6,5 @@ role_type 'ArchiveAccessor', { role => 'EpiRR::Roles::ArchiveAccessor' };
 role_type 'HasErrors',       { role => 'EpiRR::Roles::HasErrors' };
 role_type 'HasMetaData',     { role => 'EpiRR::Roles::HasMetaData' };
 role_type 'HasUserAgent',    { role => 'EpiRR::Roles::HasUserAgent' };
+
+1;
