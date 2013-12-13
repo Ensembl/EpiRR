00dea80aeb341cacf7425ebb8ad730a4aa35f76e
davidrichardson <david.m.richardson@gmail.com>
2013-12-13 16:57:26 +0000

    Documentation for core module

--- lib/EpiRR.pm
+++ lib/EpiRR.pm
@@ -12,6 +12,10 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 package EpiRR;
+our $VERSION = '0.01';
+1;
+__END__
+=pod
 
 =head1 NAME
 
@@ -19,21 +23,52 @@ EpiRR - An Epigeomics reference dataset registry
 
 =head1 VERSION
 
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
+version 0.01
 
 =head1 SYNOPSIS
 
+perl scripts/accession_dataset.pl -file dataset.file > dataset.file.out
 
-=cut
+=head1 DESCRIPTION
 
+Accession reference datasets, consisting of raw data in archives
+
+=head1 ACKNOWLEDGEMENTS
+
+=over 4
+
+=item Ian Streeter, for the BioSD module
+
+=item Andy Yates, for excellent advice on modern perl
+
+=back
+
+=head1 BUGS
+
+All complex software has bugs lurking in it, and this module is no
+exception.
+
+Please report any bugs to C<davidr@ebi.ac.uk>, or through the web
+interface at L<http://rt.cpan.org>.
 
 =head1 AUTHOR
 
 David Richardson, C<< <davidr at ebi.ac.uk> >>
 
-1;
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2013 European Molecular Biology Laboratory - European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
