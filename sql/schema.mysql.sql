6ee8921f0b937cea129fa8caa03d0d4271fbb4bb
Agathe Delaune <agathedelaune@yahoo.fr>
2017-12-06 15:06:23 +0000

    modified the order

--- sql/schema.mysql.sql
+++ sql/schema.mysql.sql
@@ -1,5 +1,5 @@
 drop table if exists meta_data;
-drop table if exists raw_meta_data;
+#drop table if exists raw_meta_data;
 drop table if exists raw_data;
 drop table if exists dataset_version;
 drop table if exists dataset;
@@ -88,18 +88,6 @@ key(dataset_version_id),
 foreign key (dataset_version_id) references dataset_version(dataset_version_id)
 i ENGINE=InnoDB CHARSET=UTF8;
 
-#raw_meta_data used to describe the experiment used in the dataset_version
-create table raw_meta_data (
-raw_meta_id int(10) unsigned not null auto_increment,
-raw_data_id int(10) unsigned not null,
-name varchar(256) not null,
-value varchar(4000) not null,
-primary key (raw_meta_id),
-key(raw_data_id),
-foreign key (raw_data_id) references raw_data(raw_data_id)
-) ENGINE=InnoDB CHARSET=UTF8;
-
-
 #raw data for a data set. points to archive entries. Most need a single id (primary accession), but this isnt sufficient for all cases so we have a secondary_accession 
 create table raw_data (
 raw_data_id int(10) unsigned not null auto_increment,
@@ -115,6 +103,17 @@ key(dataset_version_id),
 foreign key (dataset_version_id) references dataset_version(dataset_version_id),
 key(archive_id),
 foreign key (archive_id) references archive(archive_id)
+) ENGINE=InnoDB CHARSET=UTF8;
+
+#raw_meta_data used to describe the experiment used in the dataset_version
+create table raw_meta_data (
+raw_meta_id int(10) unsigned not null auto_increment,
+raw_data_id int(10) unsigned not null,
+name varchar(256) not null,
+value varchar(4000) not null,
+primary key (raw_meta_id),
+key(raw_data_id),
+foreign key (raw_data_id) references raw_data(raw_data_id)
 ) ENGINE=InnoDB CHARSET=UTF8;
 
 
