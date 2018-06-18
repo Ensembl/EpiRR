ed6cb2800ddc8a7fca81a63c9f5118ab1a6e0307
Myrto Kostadima <kostadim@ebi.ac.uk>
2018-06-18 11:50:50 +0100

    rejecting juettamann's last commits and resubmitting validate_xmls.pl

--- scripts/validate_xmls.pl
+++ scripts/validate_xmls.pl
@@ -24,24 +24,20 @@ use File::Basename;
 use autodie;
 use feature qw(say);
 use Data::Dumper;
-use URI::Encode qw(uri_encode);
-use LWP;
-#has '+base_url'        => ( default => 'https://www.ebi.ac.uk/ega/datasets/' );  
-#has '+valid_status_id' => ( default => 2 );
-
 $Data::Dumper::Indent = 1;
 $Data::Dumper::Sortkeys = 1;
 $Data::Dumper::Deepcopy = 1;
-
 
 my $config_module = 'EpiRR::Config';
 my $dir;
 my $outfile;
 my $quiet = 0;
+my $python_script_dir; 
 
 GetOptions(
   "config=s" => \$config_module,
   "dir=s"    => \$dir,
+  "python_script_dir=s"    => \$python_script_dir,
   'quiet!'   => \$quiet,
 ) or croak("Error with options: $!");
 
@@ -55,183 +51,110 @@ my $report_file_name = "$dir/summary." . time . ".tsv";
 open my $r_fh, '>', $report_file_name;
 print $r_fh "EpiRR accession\tProject name\tExperiment/Sample ID\tValidates?\tErrors\n";
 
-my $errors = [];
+my $container = $config_module->c();
 my $database_service = $container->resolve( service => 'database/dbic_schema' );
 my $conversion_service = $container->resolve( service => 'conversion_service' );
-my $acs = {};
-$acs->{EGA} = $conversion_service->get_accessor('EGA');
-$acs->{ENA} = $conversion_service->get_accessor('ENA');
-$acs->{DDBJ} = $conversion_service->get_accessor('DDBJ');
-
 my @all_raw_data = $database_service->raw_data->all;
 
 my $count=0;
 foreach my $raw_data(@all_raw_data){
+  if ( $raw_data->archive->name eq 'EGA' || $raw_data->archive->name eq 'ENA' || $raw_data->archive->name eq 'DDBJ' ) {
+    my $project_name=$raw_data->dataset_version->dataset->project->name;
+    my $epirr_accession=$raw_data->dataset_version->dataset->accession;
 
-  my $archive_name = $raw_data->archive->name;
-  next unless(exists  $acs->{$archive_name});
-  next unless($raw_data->dataset_version->is_current);
+    if ( $raw_data->dataset_version->is_current ) {
+      my $errors = [];
+      my @list_of_EGAX = ($raw_data->primary_accession,);
 
-  my $ac = $acs->{$archive_name};
-  foreach my $n ($raw_data->primary_accession) {
-    my $experiment = $ac->experiment_xml($n , $errors);
-    say $archive_name;die;
-#    my ( $exp_id, $status_id, $experiment_xml ) = @$row;
-    say Dumper($experiment); die;
-    if( $n eq "EGAX00001272789") {
+      foreach my $n (@list_of_EGAX) {
+        #if( $n eq "EGAX00001147727" || $n eq "EGAX00001272789" || $n eq "EGAX00001272563" || $n eq "EGAX00001169503" ) {
+        #if ( $count <= 10 ) {
+        #if( $n eq "EGAX00001273373" || $n eq "EGAX00001273399" ) {
+        my $accessor = $conversion_service->get_accessor( $raw_data->archive->name );
+                print "\n-------------------------\n";
+        print "Experiment: $n\n";
 
+        my $exp_row = $accessor->experiment_xml( $n, $errors );
+        #print Dumper($exp_row); 
+                my ( $exp_id, $status_id, $experiment_xml ) = @$exp_row;
 
-#	my $row = $self->experiment_xml( $n, $errors );
-#	my ( $exp_id, $status_id, $experiment_xml ) = @$row;
-#        
-#	open my $exp_xml_fh, '>', "$exp_id.xml";
-#	print $exp_xml_fh $experiment_xml."\n";
-#        close $exp_xml_fh;
-#
-#        my $sample_row = $self->lookup_sample( $experiment->sample_id(), $errors ) if ($experiment->sample_id());
-#	my ( $sample_id, $sample_xml ) = @$row;
-#
-#	open my $sample_xml_fh, '>', "$sample_id.xml";
-#	print $sample_xml_fh $sample_xml."\n";
-#        close $sample_xml_fh;
-#
-#        my $cmd = "pipenv run main.py -sample -out:./".$sample_id.".versioned.xml ./".$sample_id.".xml";
-#	system$cmd);
+        open my $exp_xml_fh, '>', "$exp_id.xml";
+        binmode($exp_xml_fh, ":utf8");
+        print $exp_xml_fh $experiment_xml;
+        close $exp_xml_fh;
 
-###  if ( $raw_data->archive->name eq 'EGA' || $raw_data->archive->name eq 'ENA' || $raw_data->archive->name eq 'DDBJ' ) {
-###    my $project_name=$raw_data->dataset_version->dataset->project->name;
-###    my $epirr_accession=$raw_data->dataset_version->dataset->accession;
-###
-###    if ( $raw_data->dataset_version->is_current ) {
-###      my $errors = [];
-###      my @list_of_EGAX = ($raw_data->primary_accession,);
-###
-###      foreach my $n (@list_of_EGAX) {
-###	#if( $n eq "EGAX00001147727" || $n eq "EGAX00001272789" || $n eq "EGAX00001272563" || $n eq "EGAX00001169503" ) {
-###        #if ( $count <= 1000 ) {
-###	my $accessor = $conversion_service->get_accessor( $raw_data->archive->name );
-###        	
-###        print "\n-------------------------\n";
-###        print "Experiment: $n\n";
-###
-###	my $exp_row = $accessor->experiment_xml( $n, $errors );
-###        my ( $exp_id, $status_id, $experiment_xml ) = @$exp_row; 
-###        
-###        open my $exp_xml_fh, '>', "$exp_id.xml";
-###        binmode($exp_xml_fh, ":utf8");
-###	print $exp_xml_fh $experiment_xml;
-###        close $exp_xml_fh;
-###
-###        my $cmd = "python lib/ihec-ecosystems/version_metadata/__main__.py -experiment -config:lib/ihec-ecosystems/version_metadata/config_edited.json -out:./".$exp_id.".versioned.xml ./".$exp_id.".xml";# 2> /dev/null";
-###	system($cmd);
-###
-###        my $exp_val_flag="False"; my $exp_val_errors=""; my @exp_val_log_files=glob("errs.experiment*.log");
-###        if ( scalar @exp_val_log_files == 1) {
-###          foreach my $exp_val_log_file ( glob("errs.experiment*.log") ) {
-###            #open my $exp_val_log_file_fh, '<', $exp_val_log_file;
-###            #while(my $line=<$exp_val_log_file_fh>) {
-###            #  if ($line =~ m/Failed validating/) {
-###            #    last;
-###            #  } else {
-###            #    chomp($line);
-###                #$line="not valid under any of the given schemas" if ( $line =~ m/is not valid under any of the given schemas/);
-###  
-###             #   $exp_val_errors.="$line; " if ( $line ne "");
-###             # }
-###            #}
-###            #close($exp_val_log_file_fh);
-###
-###            system("rm $exp_val_log_file");
-###          }
-###
-###          system("rm ./".$exp_id.".versioned.xml");
-###          $cmd = "python lib/ihec-ecosystems/version_metadata/__main__.py -experiment -extract -config:lib/ihec-ecosystems/version_metadata/config_edited.json -out:./".$exp_id.".versioned.xml ./".$exp_id.".xml"; #2> /dev/null";
-###          system($cmd);
-###
-###          $cmd = "python lib/ihec-ecosystems/version_metadata/review.py -sample ./".$exp_id.".xml.extracted.json";
-###
-###          my $review_xml_output = `$cmd`;
-###          chomp $review_xml_output;
-###
-###          #$review_xml_output=~ s/[u*\[\]]//g;
-###
-###          #$exp_val_errors.="Missing ".$review_xml_output;
-###          $exp_val_errors.=$review_xml_output;
-###        } elsif ( scalar @exp_val_log_files > 1 ) {
-###          print "Found too many experiment log files: ".scalar @exp_val_log_files."\n";
-###          exit;
-###        } else { $exp_val_flag="True"; }
-###      
-###        print $r_fh "$epirr_accession\t$project_name\t$exp_id\t$exp_val_flag\t$exp_val_errors\n";
-###        system("rm -f ./".$exp_id.".versioned.xml ./".$exp_id.".xml.extracted.json ./".$exp_id.".xml");
-### 
-###        my $experiment = $accessor->extract_metadata_from_experiment_xml( $exp_row, $errors ); 
-###        
-###        my $sample_row = $accessor->lookup_sample( $experiment->sample_id(), $errors ) if ($experiment->sample_id());
-###	my ( $sample_id, $sample_xml ) = @$sample_row;
-###
-###        my $sample = $accessor->xml_parser()->parse_sample( $sample_xml, $errors );        
-###
-###        print "\n-------------------------\n";
-###        print "Sample: $sample_id\n";
-###
-###	open my $sample_xml_fh, '>', "$sample_id.xml";
-###	binmode($sample_xml_fh, ":utf8");
-###        print $sample_xml_fh $sample_xml."\n";
-###        close $sample_xml_fh;
-###
-###        $cmd = "python lib/ihec-ecosystems/version_metadata/__main__.py -sample -config:lib/ihec-ecosystems/version_metadata/config_edited.json -out:./".$sample_id.".versioned.xml ./".$sample_id.".xml"; #2> /dev/null";
-###	system($cmd);
-###
-###        my $sample_val_flag="False"; my $sample_val_errors=""; my @sample_val_log_files=glob("errs.sample*.log");
-###
-###	$sample_val_errors.="Missing 'MOLECULE'; " if ( ! ( $sample->get_meta_data('molecule') || $experiment->get_meta_data('molecule')));
-###        if ( scalar @sample_val_log_files == 1) {
-###
-###          foreach my $sample_val_log_file ( @sample_val_log_files ) {
-###            system("rm $sample_val_log_file");
-###          }  
-###            #print "\n-------------------------------------\n";
-###            #print "Reading from file: $sample_val_log_file\n";
-###            #print "\n-------------------------------------\n";
-###
-###          #  open my $sample_val_log_file_fh, '<', $sample_val_log_file;
-###          #  while(my $line=<$sample_val_log_file_fh>) {
-###          #    if ($line =~ m/^Failed validating/) {
-###          #      last;
-###          #    } else {
-###          #      chomp($line);
-###                #print "\n$line\n";
-###                #$line="not valid under any of the given schemas" if ( $line =~ m/is not valid under any of the given schemas/);
-###          #      $sample_val_errors.="$line; " if ( $line ne "" );
-###          #    }
-###          #  }
-###          #  close($sample_val_log_file_fh);
-###
-###          system("rm ./".$sample_id.".versioned.xml");
-###          $cmd = "python lib/ihec-ecosystems/version_metadata/__main__.py -sample -extract -config:lib/ihec-ecosystems/version_metadata/config_edited.json -out:./".$sample_id.".versioned.xml ./".$sample_id.".xml"; #2> /dev/null";
-###          system($cmd);
-###
-###          $cmd = "python lib/ihec-ecosystems/version_metadata/review.py -sample ./".$sample_id.".xml.extracted.json";
-###
-###          my $review_xml_output = `$cmd`;
-###          chomp $review_xml_output;
-###
-###          #$review_xml_output=~ s/[u*\[\]]//g;
-###
-###          $sample_val_errors.=$review_xml_output;
-###          #$sample_val_errors.="Missing ".$review_xml_output;
-###        } elsif ( scalar @sample_val_log_files > 1 ) { 
-###          print "Found too many log files: ".scalar @sample_val_log_files."\n";
-###          exit;
-###        } elsif ( $sample_val_errors eq "" ) { $sample_val_flag="True"; }
-###
-###        print $r_fh "$epirr_accession\t$project_name\t$sample_id\t$sample_val_flag\t$sample_val_errors\n";
-###        system("rm -f ./".$sample_id.".versioned.xml ./".$sample_id.".xml.extracted.json ./".$sample_id.".xml");
-###      
-###        #$count+=1;
-###      }#}
-###>>>>>>> 71ca20a95f9138f234027c276450c37269155d56
+        my $cmd = "python $python_script_dir/ihec-ecosystems/version_metadata/__main__.py -experiment -config:lib/ihec-ecosystems/version_metadata/config_edited.json -out:./".$exp_id.".versioned.xml ./".$exp_id.".xml";# 2> /dev/null";
+        system($cmd);
+
+        my $exp_val_flag="False"; my $exp_val_errors=""; my @exp_val_log_files=glob("errs.experiment*.log");
+        if ( scalar @exp_val_log_files == 1) {
+          print "Found error logs when validating: ".@exp_val_log_files."\n";
+          foreach my $exp_val_log_file ( glob("errs.experiment*.log") ) {
+
+            system("rm $exp_val_log_file");
+          }
+
+          system("rm ./".$exp_id.".versioned.xml");
+          $cmd = "python $python_script_dir/ihec-ecosystems/version_metadata/__main__.py -experiment -extract -config:lib/ihec-ecosystems/version_metadata/config_edited.json -out:./".$exp_id.".versioned.xml ./".$exp_id.".xml"; #2> /dev/null";/
+          system($cmd);
+
+          $cmd = "python $python_script_dir/ihec-ecosystems/version_metadata/review.py -sample ./".$exp_id.".xml.extracted.json";
+
+          my $review_xml_output = `$cmd`;
+          chomp $review_xml_output;
+          $exp_val_errors.=$review_xml_output;
+        } elsif ( scalar @exp_val_log_files > 1 ) {
+          print "Found too many experiment log files: ".scalar @exp_val_log_files."\n";
+          exit;
+        } else { print "Experiment $exp_id validates!\n"; $exp_val_flag="True"; }
+
+        print $r_fh "$epirr_accession\t$project_name\t$exp_id\t$exp_val_flag\t$exp_val_errors\n";
+        system("rm -f ./".$exp_id.".versioned.xml ./".$exp_id.".xml.extracted.json ./".$exp_id.".xml");
+
+        my $experiment = $accessor->extract_metadata_from_experiment_xml( $exp_row, $errors );
+
+        my $sample_row = $accessor->lookup_sample( $experiment->sample_id(), $errors ) if ($experiment->sample_id());
+        my ( $sample_id, $sample_xml ) = @$sample_row;
+
+        my $sample = $accessor->xml_parser()->parse_sample( $sample_xml, $errors );
+
+        print "\n-------------------------\n";
+        print "Sample: $sample_id\n";
+
+        open my $sample_xml_fh, '>', "$sample_id.xml";
+        binmode($sample_xml_fh, ":utf8");
+        print $sample_xml_fh $sample_xml."\n";
+        close $sample_xml_fh;
+
+        $cmd = "python $python_script_dir/ihec-ecosystems/version_metadata/__main__.py -sample -config:lib/ihec-ecosystems/version_metadata/config_edited.json -out:./".$sample_id.".versioned.xml ./".$sample_id.".xml"; #2> /dev/null";
+        system($cmd);
+
+        my $sample_val_flag="False"; my $sample_val_errors=""; my @sample_val_log_files=glob("errs.sample*.log");
+
+        $sample_val_errors.="Missing 'MOLECULE'; " if ( ! ( $sample->get_meta_data('molecule') || $experiment->get_meta_data('molecule')));
+        if ( scalar @sample_val_log_files == 1) {
+
+          foreach my $sample_val_log_file ( @sample_val_log_files ) {
+            system("rm $sample_val_log_file");
+          }
+              system("rm ./".$sample_id.".versioned.xml");
+          $cmd = "python $python_script_dir/ihec-ecosystems/version_metadata/__main__.py -sample -extract -config:lib/ihec-ecosystems/version_metadata/config_edited.json -out:./".$sample_id.".versioned.xml ./".$sample_id.".xml"; #2> /dev/null";
+          system($cmd);
+
+          $cmd = "python $python_script_dir/ihec-ecosystems/version_metadata/review.py -sample ./".$sample_id.".xml.extracted.json";
+          my $review_xml_output = `$cmd`;
+          chomp $review_xml_output;
+          $sample_val_errors.=$review_xml_output;
+        } elsif ( scalar @sample_val_log_files > 1 ) {
+          print "Found too many log files: ".scalar @sample_val_log_files."\n";
+          exit;
+        } elsif ( $sample_val_errors eq "" ) { $sample_val_flag="True"; }
+
+        print $r_fh "$epirr_accession\t$project_name\t$sample_id\t$sample_val_flag\t$sample_val_errors\n";
+        system("rm -f ./".$sample_id.".versioned.xml ./".$sample_id.".xml.extracted.json ./".$sample_id.".xml");
+
+        #$count+=1;
+      }#}
     }
   }
 }
