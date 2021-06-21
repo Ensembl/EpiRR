d1383994adc7d5eba9b1593c9438b906fe42df4c
Thomas Juettemann <juettemann@gmail.com>
2021-06-21 18:21:28 +0100

    XML validations and conversion

--- epirr/metadata_validation/read_file.py
+++ epirr/metadata_validation/read_file.py
@@ -1,4 +1,5 @@
-import sys, getopt
+import sys, getopt, xmltodict, json
+from XmlTest import XmlTest
 
 def main(argv):
     inputfile = ''
@@ -18,8 +19,40 @@ def main(argv):
             sys.exit()
             
     with open(inputfile, 'r') as file:
-        data = file.read().replace('\n', '')
-    print(data)
+        return file.read()
+   
+
+def parse(file):
+        parser = None
+        # find parser
+        try:
+            from lxml.etree import parse, XMLSchema, fromstring
+            print('using lxml.etree parser')
+            # parse XML and validate it
+            tree = fromstring(file)
+            # get XSD
+            schemaDoc = parse('xsd/SRA.sample.xsd')
+            schema = XMLSchema(schemaDoc)
+            if schema.validate(tree):
+                print('XML validated')
+                return tree
+            print(schema.error_log)
+            raise ValueError('XML NOT validated')
+        except ImportError:
+            try:
+                from xml.etree.ElementTree import fromstring
+                print('using xml.etree.ElementTree parser')
+                return fromstring(file)
+            except ImportError:
+                print("Failed to import ElementTree from any known place")
+                raise 
+
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    file = main(sys.argv[1:])
+    xml_test = XmlTest(file)
+    xml_test.check_xml_identify_type()
+    xml_test.validate()
+    o = xml_test.convert_xml_to_json()
+    print(type(o))
+    print(json.dumps(o,indent=4))
